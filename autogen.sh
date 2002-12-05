883c22ce26e8a8b3023d9ef3a263ca3396214ee1
Thien-Thi Nguyen <ttn@gnuvola.org>
2002-12-05 21:52:14 +0000

    Take "--libtoolize" option; conditionalize libtoolize invocation. Pass "-I ." to aclocal. Pass "--force" to automake. Record dev box tool versions in comment.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,16 @@
-#! /bin/sh
-# $Id$
-aclocal -I .
-libtoolize --copy --automake
+#!/bin/sh
+# Usage: sh -x autogen.sh [--libtoolize]
+#
+# Bootstrap the development environment.  Optional arg "--libtoolize" means
+# also run "libtoolize --force".  The distribution was made w/ these tools:
+# - libtool 1.4.3
+# - autoconf 2.53
+# - automake 1.6.3
+
+test x"$1" = x--libtoolize && libtoolize --force
+aclocal
 autoheader
 autoconf
-automake --add-missing
+automake --add-missing --force
 
+# autogen.sh ends here
