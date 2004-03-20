f24ef802a9a2fc45ce784f576dd16975c6db7e6a
Thien-Thi Nguyen <ttn@gnuvola.org>
2004-03-20 01:09:44 +0000

    Use new procs in testing.scm. Rename procs to avoid "pg-" prefix. Rename some top-level vars for legibility. No longer use diff(1) to check invariance; instead, compare character-by-character.

--- test/guile-pg-lo-tests.scm
+++ test/guile-pg-lo-tests.scm
@@ -1,40 +1,49 @@
-;;    Guile-pg - A Guile interface to PostgreSQL
-;;    Copyright (C) 1999-2000,02,03,2004 Free Software Foundation, Inc.
-;;
-;;    This program is free software; you can redistribute it and/or modify
-;;    it under the terms of the GNU General Public License as published by
-;;    the Free Software Foundation; either version 2 of the License, or
-;;    (at your option) any later version.
-;;
-;;    This program is distributed in the hope that it will be useful,
-;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;    GNU General Public License for more details.
-;;
-;;    You should have received a copy of the GNU General Public License
-;;    along with this program; if not, write to the Free Software
-;;    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+;;; guile-pg-lo-tests.scm
 
-;;    Author: Ian Grant <Ian.Grant@cl.cam.ac.uk>
+;;	Copyright (C) 1999,2000,02,03,2004 Free Software Foundation, Inc.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+(load-from-path (in-vicinity *srcdir* "testing.scm"))
+(reset-all-tests!)
 
 (use-modules (database postgres))
 
+;; We use one connection for all the tests.
+
+(define *C* #f)
+
+;; We also want to keep track of the OID
+(define *O* #f)
+
 ;; Define some basic procedures
 
-;; pg-exec-cmd CONN
-;; Execute a command on CONN, return #t if OK, #f otherwise
-(define (pg-exec-cmd conn sql)
-  (let ((res (pg-exec conn sql)))
+;; cexec SQL
+;; Execute SQL command on `*C*', return #t if OK, #f otherwise.
+(define (cexec sql)
+  (let ((res (pg-exec *C* sql)))
     (and res (eq? 'PGRES_COMMAND_OK (pg-result-status res)))))
 
-;; pg-transaction CONN PROC ARGS
-;; Return the result of evaluating PROC with arguments (cons CONN ARGS)
-;;   within a postgres transaction, or #f in the event of an error
-(define (pg-transaction conn proc . args)
-  (and (pg-exec-cmd conn "BEGIN TRANSACTION")
-       (let* ((res-apply (apply proc (cons conn args)))
-              (res-end (pg-exec-cmd conn "END TRANSACTION")))
-         (and res-end res-apply))))
+;; transaction THUNK
+;; Expand into an expression that returns the result of evaluating BODY forms
+;; within a PostgreSQL transaction, or #f in the event of an error.
+(defmacro transaction body
+  `(and (cexec "BEGIN TRANSACTION")
+        (let* ((res-apply ((lambda () ,@body)))
+               (res-end (cexec "END TRANSACTION")))
+          (and res-end res-apply))))
 
 ;; eval-port PORT
 ;; Like (eval-string) but reads from PORT
@@ -46,27 +55,19 @@
           (iter (eval-in-module form (current-module))))))
   (iter #f))
 
-;; We want to keep one connection hanging around for the duration of all the
-;; tests
+;; Here we define procedures to carry out the tests.
 
-(define conn #f)
+(define test:make-connection
+  (add-test #t
+    (lambda ()
+      (->bool (false-if-exception (set! *C* (pg-connectdb "")))))))
 
-;; We also want to keep track of the OID
-(define oid #f)
+(define test:make-table
+  (add-test #t
+    (lambda ()
+      (cexec "CREATE TABLE test (col1 int4, col2 oid)"))))
 
-;; Test pg-connectdb
-;; expect #t
-(define (test:make-connection)
-  (->bool (false-if-exception (set! conn (pg-connectdb "")))))
-
-;; Test pg-exec
-;; expect #f
-(define (test:make-table)
-  (pg-exec-cmd conn "CREATE TABLE test (col1 int4, col2 oid)"))
-
-;; Tests nothing
-;; expect a longish string containing valid scheme
-(define test:data '("
+(define some-test-data '("
 ;; This is a data file for use by guile-pg-lo-tests.scm
 ;;   it will be imported to a large onject and then
 ;;   evaluated directly from the large object
@@ -75,166 +76,169 @@
 
 ;; End of test data"))
 
-;; Tests nothing
-;; expect #t
-(define (test:make-data)
-  (let ((fport (open-file "lo-tests-data-1" "w")))
-    (and fport
-         (begin
-           (for-each (lambda (string) (write-line string fport)) test:data)
-           (close-port fport)))))
+(define test:make-data
+  (add-test #t
+    (lambda ()
+      (let ((fport (open-file "lo-tests-data-1" "w")))
+        (and fport
+             (begin
+               (for-each (lambda (string)
+                           (display string fport))
+                         some-test-data)
+               (close-port fport)))))))
 
-;; Test pg-lo-import
-;; expect #t
-(define (test:lo-import)
-  (and (pg-transaction conn
-         (lambda (conn)
-           (set! oid (pg-lo-import conn "lo-tests-data-1"))))
-       (->bool oid)))
+(define test:lo-import
+  (add-test #t
+    (lambda ()
+      (and (transaction
+            (set! *O* (pg-lo-import *C* "lo-tests-data-1")))
+           (->bool *O*)))))
 
-;; Test pg-lo-export
-;; expect #t
-(define (test:lo-export)
-  (false-if-exception (delete-file "lo-tests-data-2"))
-  (pg-transaction conn
-    (lambda (conn)
-      (and (pg-lo-export conn oid "lo-tests-data-2")
-           (eq? 0 (system (format #f "~A ~A ~A >/dev/null 2>&1"
-                                  *DIFF-PATH*
-                                  "lo-tests-data-1"
-                                  "lo-tests-data-2")))))))
+(define test:lo-export
+  (add-test #t
+    (lambda ()
+      (false-if-exception (delete-file "lo-tests-data-2"))
+      (transaction
+       (and (pg-lo-export *C* *O* "lo-tests-data-2")
+            ;; poor man's cmp(1)
+            (let* ((expected (apply string-append some-test-data))
+                   (len (string-length expected))
+                   (p (open-input-file "lo-tests-data-2"))
+                   (next (lambda () (read-char p))))
+              (let loop ((c (next)) (i 0))
+                (cond ((eof-object? c)
+                       (close-port p)
+                       (= i len))       ; exact match required
+                      ((= i len)
+                       (close-port p)
+                       #f)              ; too much
+                      (else
+                       (and (char=? c (string-ref expected i))
+                            (loop (next) (1+ i))))))))))))
 
-;; Test pg-lo-open-read
-;; expect #t
-(define (test:lo-open-read)
-  (pg-transaction conn
-    (lambda (conn)
-      (let ((lo-port (pg-lo-open conn oid "r")))
-        (and lo-port
-             (eq? conn (pg-lo-get-connection lo-port))
-             (equal? (eval-port lo-port) '(testing testing one two three))
-             (close-port lo-port))))))
-
-;; Test pg-lo-creat
-;; expect #t
+(define test:lo-open-read
+  (add-test #t
+    (lambda ()
+      (transaction
+       (let ((lo-port (pg-lo-open *C* *O* "r")))
+         (and lo-port
+              (eq? *C* (pg-lo-get-connection lo-port))
+              (equal? (eval-port lo-port) '(testing testing one two three))
+              (close-port lo-port)))))))
 
 (define nchars 100)
-(define new-oid #f)
+(define *N* #f)                         ; new oid
 
 (define (write-chars n c lop)
   (do ((i 0 (1+ i)))
       ((= i n) #t)
     (write-char c lop)))
 
-(define (test:lo-creat)
-  (pg-transaction conn
-    (lambda (conn)
-      (let ((lo-port (pg-lo-creat conn "w")))
-        (and lo-port
-             (write-chars nchars #\a lo-port)
-             (set! new-oid (pg-lo-get-oid lo-port))
-             (close-port lo-port))))))
+(define test:lo-creat
+  (add-test #t
+    (lambda ()
+      (transaction
+       (let ((lo-port (pg-lo-creat *C* "w")))
+         (and lo-port
+              (write-chars nchars #\a lo-port)
+              (set! *N* (pg-lo-get-oid lo-port))
+              (close-port lo-port)))))))
 
-;; Test pg-lo-read
-;; expect #t
+(define test:lo-read
+  (add-test #t
+    (lambda ()
+      (transaction
+       (let ((lo-port (pg-lo-open *C* *N* "r"))
+             (data #f))
+         (and lo-port
+              (set! data (pg-lo-read 1 nchars lo-port))
+              (close-port lo-port)
+              (equal? data (make-string nchars #\a))))))))
 
-(define (test:lo-read)
-  (pg-transaction conn
-    (lambda (conn)
-      (let ((lo-port (pg-lo-open conn new-oid "r"))
-            (data #f))
-        (and lo-port
-             (set! data (pg-lo-read 1 nchars lo-port))
-             (close-port lo-port)
-             (equal? data (make-string nchars #\a)))))))
+(define test:read-line
+  (add-test #t
+    (lambda ()
+      (transaction
+       (let ((lo-port (pg-lo-open *C* *N* "r"))
+             (data #f))
+         (and lo-port
+              (set! data (read-line lo-port))
+              (close-port lo-port)
+              (equal? data (make-string nchars #\a))))))))
 
-;; Test read-line
-;; expect #t
-
-(define (test:read-line)
-  (pg-transaction conn
-    (lambda (conn)
-      (let ((lo-port (pg-lo-open conn new-oid "r"))
-            (data #f))
-        (and lo-port
-             (set! data (read-line lo-port))
-             (close-port lo-port)
-             (equal? data (make-string nchars #\a)))))))
-
-;; Test pg-lo-seek
-;; expect #t
-
-(define (test:lo-seek)
-  (pg-transaction conn
-    (lambda (conn)
-      (let* ((trace-port (open-file "test-lo-seek.log" "w"))
-             (lo-port (pg-lo-open conn new-oid "w")))
-        (and (pg-trace conn trace-port)
-             lo-port
-             (eq? (pg-lo-seek lo-port 1 SEEK_SET) 1)
-             (write-char #\b lo-port)
-             (force-output lo-port)
-             (eq? (pg-lo-seek lo-port 3 SEEK_SET) 3)
-             (write-char #\b lo-port)
-             (force-output lo-port)
-             (eq? (pg-lo-seek lo-port 0 SEEK_SET) 0)
-             (close-port lo-port)
-             (pg-untrace conn)
-             (close-port trace-port))))))
+(define test:lo-seek
+  (add-test #t
+    (lambda ()
+      (transaction
+       (let* ((trace-port (open-file "test-lo-seek.log" "w"))
+              (lo-port (pg-lo-open *C* *N* "w")))
+         (and (pg-trace *C* trace-port)
+              lo-port
+              (eq? (pg-lo-seek lo-port 1 SEEK_SET) 1)
+              (write-char #\b lo-port)
+              (force-output lo-port)
+              (eq? (pg-lo-seek lo-port 3 SEEK_SET) 3)
+              (write-char #\b lo-port)
+              (force-output lo-port)
+              (eq? (pg-lo-seek lo-port 0 SEEK_SET) 0)
+              (close-port lo-port)
+              (pg-untrace *C*)
+              (close-port trace-port)))))))
 
 ;; Test pg-lo-seek pg-lo-read
 ;; expect "ababaa"
 
-(define (test:lo-seek2)
-  (pg-transaction conn
-    (lambda (conn)
-      (let ((lo-port (pg-lo-open conn new-oid "r"))
-            (data #f))
-        (and lo-port
-             (set! data (pg-lo-read 1 6 lo-port))
-             (close-port lo-port)
-             data)))))
+(define test:lo-seek2
+  (add-test "ababaa"
+    (lambda ()
+      (transaction
+       (let ((lo-port (pg-lo-open *C* *N* "r"))
+             (data #f))
+         (and lo-port
+              (set! data (pg-lo-read 1 6 lo-port))
+              (close-port lo-port)
+              data))))))
 
-;; Test pg-lo-tell
-;; expect #t
+(define test:lo-tell
+  (add-test #t
+    (lambda ()
+      (transaction
+       (let ((lo-port (pg-lo-open *C* *N* "r")))
+         (and lo-port
+              (eq? 0 (pg-lo-tell lo-port))
+              (let ((data (pg-lo-read 1 1 lo-port)))
+                (and (string? data)
+                     (= 1 (string-length data))
+                     (string=? "a" data)))
+              (let ((location-after-read (pg-lo-tell lo-port)))
+                (eq? 1 (pg-lo-seek lo-port 0 SEEK_CUR))
+                (close-port lo-port)
+                (eq? 1 location-after-read))))))))
 
-(define (test:lo-tell)
-  (pg-transaction conn
-    (lambda (conn)
-      (let ((lo-port (pg-lo-open conn new-oid "r")))
-        (and lo-port
-             (eq? 0 (pg-lo-tell lo-port))
-             (let ((data (pg-lo-read 1 1 lo-port)))
-               (and (string? data)
-                    (= 1 (string-length data))
-                    (string=? "a" data)))
-             (let ((location-after-read (pg-lo-tell lo-port)))
-               (eq? 1 (pg-lo-seek lo-port 0 SEEK_CUR))
-               (close-port lo-port)
-               (eq? 1 location-after-read)))))))
+(define (main)
+  (set! verbose #t)
+  (test-init "lo-tests" 13)
+  (test *VERSION* pg-guile-pg-version)
+  (test! test:make-connection
+         test:make-data
+         test:make-table
+         test:lo-import
+         test:lo-export
+         test:lo-open-read
+         test:lo-creat
+         test:lo-read
+         test:read-line
+         test:lo-seek
+         test:lo-seek2
+         test:lo-tell)
+  (for-each delete-file '("lo-tests-data-1" "lo-tests-data-2"))
+  (set! *C* #f)
+  (test-report))
 
-(load-from-path (string-append *srcdir* "/testing.scm"))
-(set! verbose #t)
-(test-init "lo-tests" 13)
-(test *VERSION* pg-guile-pg-version)
-(test #t test:make-connection)
-(test #t test:make-data)
-(test #t test:make-table)
-(test #t test:lo-import)
-(test #t test:lo-export)
-(test #t test:lo-open-read)
-(test #t test:lo-creat)
-(test #t test:lo-read)
-(test #t test:read-line)
-(test #t test:lo-seek)
-(test "ababaa" test:lo-seek2)
-(test #t test:lo-tell)
-(for-each delete-file '("lo-tests-data-1" "lo-tests-data-2"))
-(set! conn #f)
-(exit (test-report))
+(exit (main))
 
 ;; Local Variables:
-;; eval: (put 'pg-transaction 'scheme-indent-function 1)
+;; eval: (put 'add-test 'scheme-indent-function 1)
 ;; End:
 
 ;;; guile-pg-lo-tests.scm ends here
