b445af10cda7f4b9e24adcc54476783240375975
Thien-Thi Nguyen <ttn@gnuvola.org>
2004-05-15 08:11:03 +0000

    Doc munging; nfc.

--- scm/postgres-col-defs.scm
+++ scm/postgres-col-defs.scm
@@ -21,11 +21,14 @@
 ;; This module exports the procs:
 ;;   (column-name DEF) => symbol
 ;;   (type-name DEF) => symbol
-;;   (type-options DEF) => list of strings
+;;   (type-options DEF) => list of option elements
 ;;   (objectifiers DEFS) => list of objectifier procedures
 ;;   (stringifiers DEFS) => list of stringifier procedures
 ;;
 ;; DEF is a single column definition.  DEFS is a list of column definitions.
+;; Each option element is either a string (possibly with embedded spaces),
+;; or a sub-list of numbers and/or symbols.  Typically the sub-list, if any,
+;; will be the first option element.
 
 ;;; Code:
 
@@ -40,18 +43,24 @@
             objectifiers
             stringifiers))
 
-;; Extract column name from @var{def}.
+;; Extract column name, a symbol, from @var{def}.
+;;
 (define (column-name def)
   (car def))
 
-;; Extract type name from @var{def}.
+;; Extract type name, a symbol, from @var{def}.
+;;
 (define (type-name def)
   (let ((type-info (cdr def)))
     (if (pair? type-info)
         (car type-info)
         type-info)))
 
-;; Extract type options from @var{def}.
+;; Extract type options, a list, from @var{def}.
+;; Each option element is either a string (possibly with embedded spaces),
+;; or a sub-list of numbers and/or symbols.  Typically the sub-list, if any,
+;; will be the first option element.
+;;
 (define (type-options def)
   (let ((type-info (cdr def)))
     (if (pair? type-info)
@@ -59,12 +68,14 @@
         '())))
 
 ;; Return a list of objectifiers associated with the types in @var{defs}.
+;;
 (define (objectifiers defs)
   (map dbcoltype:objectifier
        (map dbcoltype-lookup
             (map type-name defs))))
 
 ;; Return a list of stringifiers associated with the types in @var{defs}.
+;;
 (define (stringifiers defs)
   (map dbcoltype:stringifier
        (map dbcoltype-lookup
