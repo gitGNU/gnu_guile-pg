c2e23f6b65de08074f9f83bba446c78b769ed3a0
Thien-Thi Nguyen <ttn@gnuvola.org>
2004-06-24 12:07:13 +0000

    No longer load $top_builddir/scm/postgres.scm. Instead, evaluate `use-modules' form.

--- test/alive.sh
+++ test/alive.sh
@@ -4,7 +4,7 @@ if [ -z "$top_builddir" ] ; then echo $0: error: Bad env. ; exit 1 ; fi
 
 if [ x"$DEBUG" = x ] ; then debug= ; else debug='--debug' ; fi
 
-${GUILE-guile} $debug -l $top_builddir/scm/postgres.scm -c '(quit)'
+${GUILE-guile} $debug -c '(use-modules (database postgres))'
 rv=$?
 
 exit $rv
