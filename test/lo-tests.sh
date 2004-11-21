5a45d682f5eea79cbf7aa32ee6ce8d5b3b8f073f
Thien-Thi Nguyen <ttn@gnuvola.org>
2004-11-21 17:13:04 +0000

    Pass `--no-worries' for first drop.

--- test/lo-tests.sh
+++ test/lo-tests.sh
@@ -5,7 +5,7 @@ if [ -z "$top_builddir" ] ; then echo $0: error: Bad env. ; exit 1 ; fi
 drop=$top_builddir/test/drop.sh
 create=$top_builddir/test/create.sh
 
-$drop
+$drop --no-worries
 $create || exit 1
 
 if [ x"$DEBUG" = x ] ; then debug= ; else debug='--debug' ; fi
