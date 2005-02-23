2d188968c9b3aad8e0bb2d53680272fa35f95586
Thien-Thi Nguyen <ttn@gnuvola.org>
2005-02-23 14:51:55 +0000

    From module (database postgres-qcons), no longer select `make-WHERE-tree'. (fix, fsel): Rewrite these comma-commands.

--- scm/postgres-gxrepl.scm
+++ scm/postgres-gxrepl.scm
@@ -44,7 +44,6 @@
                 #:select (sql-quote
                           make-SELECT/OUT-tree
                           make-SELECT/FROM/OUT-tree
-                          make-WHERE-tree
                           parse+make-SELECT/tail-tree
                           sql-command<-trees))
   #:autoload (ice-9 pretty-print) (pretty-print)
@@ -238,63 +237,108 @@ Optional arg SETTING turns on echoing if a positive number."
 
   (defcc (fix part . set)
     "Fix query PART to be SET..., or clear that part if SET is 0 (zero).
-PART is a symbol, one of `froms', `outs', `where'.  SET is a space-separated
+PART is a keyword, one of #:cols, #:from, #:where, #:where/combiner,
+#:group-by, #:having, #:order-by, or #:limit.  SET is a space-separated
 list of elements.  When a part is fixed, it is used (unless overridden) in
 the \",fsel\" comma-command.
 
-If PART is `froms', each element of SET is either the name of a table,
-or a pair in the form (ALIAS . TABLE-NAME), e.g., `(t . pg_type)'.  The
-alias can be used in `outs' and `where' sets.
-
-If PART is `outs', each element of SET is either a column name, possibly
-qualified by the table name with a dot), e.g., `t.oid'; a prefix-style
+If PART is #:cols, each element of SET is either a column name, possibly
+qualified by the table name with a dot, e.g., `t.oid'; a prefix-style
 SQL expression, e.g., `(to_char 42 \"999\")'; or pair with the car a
 string and the cdr one of the previous options, e.g., `(\"id\" . t.oid)'.
 
-If PART is `where', each element of SET is prefix-style SQL expression,
+If PART is #:from, each element of SET is either the name of a table,
+or a pair in the form (ALIAS . TABLE-NAME), e.g., `(t . pg_type)'.  The
+alias can be used in `outs' and `where' sets.
+
+If PART is #:where, each element of SET is prefix-style SQL expression,
 .e.g, `(= t.oid 42)', and there is an implicit \"and\" clause surrounding
-SET.  For \"or\" behavior, specify it, e.g., `(or (= t.oid 42) (= t.oid 0))'."
-    (let* ((prop (case part
-                   ((froms) #:gxrepl-froms)
-                   ((outs) #:gxrepl-outs)
-                   ((where) #:gxrepl-wheres)
-                   (else #f)))
-           (cur (and prop (conn-get prop))))
-      (if (not prop)
-          (for-display "No such part, try \",help fix\"")
-          (conn-put prop (cond ((null? set) cur)
-                               ((equal? 0 (car set)) #f)
-                               (else set))))))
+SET.  For \"or\" behavior, fix #:where/combiner to be `or'.
+
+If PART is #:group-by or #:having, each element of SET is a
+prefix-style SQL expression.
+
+If PART is #:order-by, each element of SET is a list (ORDFUNC EXPR),
+where ORDFUNC is either `<', '>' or the name of an SQL function that
+takes two args and returns their ordering; and EXPR is a prefix-style
+SQL expression.
+
+If PART is #:limit, the first element of SET specifies an integer."
+    (case part
+      ((#:where/combiner)
+       (conn-put part (and (not (null? set))
+                           (memq (car set) '(and or))
+                           (car set))))
+      ((#:limit)
+       (conn-put part (and (not (null? set))
+                           (integer? (car set))
+                           (car set))))
+      ((#:cols #:from #:where #:group-by #:having #:order-by)
+       (let ((cur (and part (conn-get part))))
+         (conn-put part (cond ((null? set) cur)
+                              ((equal? 0 (car set)) #f)
+                              (else set)))))
+      (else
+       (for-display "No such part, try \",help fix\""))))
 
   (defcc (fsel . etc)
-    "Do a \"fixed-part select\", overriding `outs' and `where' from ETC.
-You must have done at least a \",fix froms TABLENAME\" before using \"fsel\".
-ETC is a series of zero or more EXPR optionally followed by a where-clause
-comprising the keyword #:where followed by a single EXPR.  EXPR is a prefix-
-style SQL expression."
-    (cond ((conn-get #:gxrepl-froms)
-           => (lambda (tables)
-                (let ((outs '())
-                      (where (and=> (conn-get #:gxrepl-wheres)
-                                    (lambda (ls)
-                                      (make-WHERE-tree
-                                       `(and ,@ls))))))
-                  (let loop ((ls etc))
-                    (cond ((null? ls))
-                          ((keyword? (car ls))
-                           (if (eq? #:where (car ls))
-                               (set! where (make-WHERE-tree (cadr ls)))))
-                          (else
-                           (set! outs (append! outs (list (car ls))))
-                           (loop (cdr ls)))))
-                  (sql-command<-trees
-                   (make-SELECT/FROM/OUT-tree
-                    tables (cond ((not (null? outs)) outs)
-                                 ((conn-get #:gxrepl-outs))
-                                 (else '*)))
-                   (or where '())))))
-          (else
-           (for-display "Sorry, no `froms' fixed, try \",help fix\""))))
+    "Do a \"fixed-part select\", overriding #:cols and other clauses from ETC.
+ETC is a series of zero or more EXPR optionally followed by an alternating
+series of keywords and EXPR.  EXPR indicates a prefix-style SQL expression."
+    (let* ((fixed (map (lambda (part)
+                         (cons part (conn-get part)))
+                       '(#:limit #:cols #:from #:where
+                                 #:group-by #:having #:order-by)))
+           (cut (let loop ((ls etc) (n 0))
+                  (if (or (null? ls) (keyword? (car ls)))
+                      n
+                      (loop (cdr ls) (1+ n)))))
+           (cols (if (< 0 cut)
+                     (list-head etc cut)
+                     (assq-ref fixed #:cols)))
+           (over (let loop ((ls (list-tail etc cut)) (acc '()))
+                   (if (null? ls)
+                       (reverse! acc)
+                       (let ((k (car ls))
+                             (v (and (not (null? (cdr ls)))
+                                     (cadr ls))))
+                         (if v
+                             (loop (cddr ls) (acons k v acc))
+                             k))))))
+      (define (o/f part)
+        (or (and=> (assq-ref over part) list)
+            (assq-ref fixed part)))
+      (define (decide part . massage)
+        (let ((v (o/f part))
+              (proc (if (null? massage)
+                        identity
+                        (car massage))))
+          (if v
+              (list part (proc v))
+              '())))
+      (cond ((keyword? over)
+             (for-display (fs "ERROR: missing value for: ~A" over)))
+            ((not cols)
+             (for-display "No columns selected"))
+            (else
+             (sql-command<-trees
+              (cond ((o/f #:from)
+                     => (lambda (from)
+                          (make-SELECT/FROM/OUT-tree from cols)))
+                    (else
+                     (list #:SELECT (make-SELECT/OUT-tree cols))))
+              (parse+make-SELECT/tail-tree
+               (append (decide #:where
+                               (lambda (v)
+                                 (if (= 1 (length v))
+                                     (car v)
+                                     (cons (or (conn-get #:where/combiner)
+                                               'and)
+                                           v))))
+                       (decide #:group-by)
+                       (decide #:having)
+                       (decide #:order-by)
+                       (decide #:limit))))))))
 
   ;; do it!
   (cond ((pg-connection? conn))
