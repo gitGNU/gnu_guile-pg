cdcf3d093f96463cc70cffbb7c3dcb155ddf9208
Thien-Thi Nguyen <ttn@gnuvola.org>
2009-06-21 12:40:03 +0200

    [C int] Update #include:d files.
    * src/libpq.c: #include "config.h" instead of <config.h>;

--- src/libpq.c
+++ src/libpq.c
@@ -19,7 +19,8 @@
    along with Guile-PG; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA  */
 
-#include <config.h>
+#include "config.h"
+#include <stdlib.h>
 #include <stdio.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
