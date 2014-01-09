76d3aa3e0323ce1bd3e099b1b9abdb12ad16e0b7
Thien-Thi Nguyen <ttn@gnuvola.org>
2014-01-09 11:15:36 +0100

    [slog] Use "snuggle/finangle.h".
    * src/libpq.c: #include "snuggle/finangle.h".
    (range_t): Delete struct.
    (RS, RLEN, FINANGLABLE_SCHEME_STRING_FROM_SYMBOL, ROZT_X)
    (_FINANGLE, UNFINANGLE, REND, NUL_AT_END_X): Delete macros.
    (_finangle) [!GI_LEVEL_NOT_YET_1_8]: Delete static func.
    (FINANGLE_RAW, FINANGLE): Delete macros.

--- src/libpq.c
+++ src/libpq.c
@@ -38,6 +38,7 @@
 #endif
 
 #include "gi.h"                         /* Guile interface */
+#include "snuggle/finangle.h"
 
 #ifdef __GNUC__
 #define UNUSED __attribute__ ((unused))
@@ -49,78 +50,6 @@
 
 #define TRUE_ENOUGH(svar)   (GIVENP (svar) && NOT_FALSEP (svar))
 
-
-/* Abstractions for Scheme objects to C string conversion.  */
-
-typedef struct {
-  char *s;
-  size_t len;
-} range_t;
-
-#define RS(svar)    c ## svar .s
-#define RLEN(svar)  c ## svar .len
-
-#if GI_LEVEL_NOT_YET_1_8
-
-#define FINANGLABLE_SCHEME_STRING_FROM_SYMBOL(sym)      \
-  scm_string_copy (scm_symbol_to_string (sym))
-
-/* Coerce a Scheme (sub)string that is to be used in contexts where the
-   extracted C string is expected to be read-only and zero-terminated.  We
-   check this condition precisely instead of simply coercing all (sub)strings,
-   to avoid waste for those substrings that may in fact (by lucky accident)
-   already satisfy the condition.  */
-#define ROZT_X(x)                                       \
-  if (SCM_ROCHARS (x) [SCM_ROLENGTH (x)])               \
-    x = BSTRING (SCM_ROCHARS (x), SCM_ROLENGTH (x))
-
-#define _FINANGLE(svar,p1)  do                  \
-    {                                           \
-      if (p1)                                   \
-        ROZT_X (svar);                          \
-      RS (svar) = SCM_ROCHARS (svar);           \
-      RLEN (svar) = SCM_ROLENGTH (svar);        \
-    }                                           \
-  while (0)
-
-#define UNFINANGLE(svar)
-
-#else  /* !GI_LEVEL_NOT_YET_1_8 */
-
-#define FINANGLABLE_SCHEME_STRING_FROM_SYMBOL  scm_symbol_to_string
-
-#define REND(svar)          RS (svar) [RLEN (svar)]
-#define NUL_AT_END_X(svar)  REND (svar) = '\0'
-
-static void
-_finangle (SCM svar, range_t *r, int p1)
-{
-  r->s = scm_to_locale_stringn (svar, &r->len);
-  if (r->s)
-    {
-      if (p1 && r->s[r->len])
-        {
-          r->s = realloc (r->s, 1 + r->len);
-          r->s[r->len] = '\0';
-        }
-    }
-  else
-    r->s = strdup ("");
-}
-
-#define _FINANGLE(svar,p1)  _finangle (svar, &c ## svar, p1)
-
-#define UNFINANGLE(svar)  free (RS (svar))
-
-#endif  /* !GI_LEVEL_NOT_YET_1_8 */
-
-/* Use ‘FINANGLE_RAW’ when the consumer of the C string takes full range
-   (start address plus length) info.  Otherwise, ‘FINANGLE’.  */
-
-#define FINANGLE_RAW(svar)  _FINANGLE (svar, 0)
-#define FINANGLE(svar)      _FINANGLE (svar, 1)
-
-
 /*
  * smob: connection
  */
